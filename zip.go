fefeeca62e0a593391fd8c145ea7af18191bbed8
yutou <138289@qq.com>
2022-03-29 12:21:45 +0800

    Update zip.go

--- zip.go
+++ zip.go
@@ -3,6 +3,7 @@ package snake
 import (
 	"archive/zip"
 	"bytes"
+	"io/fs"
 )
 
 type Ziplib struct {
@@ -19,11 +20,18 @@ func Zip(zipfile string) *Ziplib {
 	return z
 }
 
-func (z *Ziplib) Add(path string, body []byte) bool {
-	if !String(path).Find(".DS_Store", true) && !String(path).Find("__MACOSX", true) {
-		if file, err := z.FS.Create(path); err == nil {
-			_, err := file.Write(body)
-			return err == nil
+func (z *Ziplib) Add(path string, stat fs.FileInfo, body []byte) bool {
+	if !String(path).Find(".DS_Store", true) && !String(path).Find("__MACOSX", true) && !String(path).Find(".gitignore", true) && !String(path).Find(".index", true) {
+		header, _ := zip.FileInfoHeader(stat)
+		header.Name = path
+		if stat.IsDir() {
+			header.Name += "/"
+			z.FS.CreateHeader(header)
+		} else {
+			if file, err := z.FS.Create(path); err == nil {
+				_, err := file.Write(body)
+				return err == nil
+			}
 		}
 	}
 	return false
